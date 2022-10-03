edb29fc5802719128ff5ab50848539c0bce54b99
Ng Han Inn <nghaninn@gmail.com>
2022-10-03 23:34:14 +0800

    Update Dockerfile jdk version

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:jdk11 as gradleimage
+FROM gradle:jdk17 as gradleimage
 COPY . /home/gradle/source
 WORKDIR /home/gradle/source
 RUN ./gradlew build -x test
 
-FROM openjdk:11-jre-slim
+FROM openjdk:17
 EXPOSE 8080
 COPY --from=gradleimage /home/gradle/source/build/libs/*-SNAPSHOT.jar /app/app.jar
 WORKDIR /app
--- src/main/kotlin/com/nghaninn/thales/AirlabApplication.kt
+++ src/main/kotlin/com/nghaninn/thales/AirlabApplication.kt
@@ -2,10 +2,15 @@ package com.nghaninn.thales
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.lang.Exception
 
 @SpringBootApplication
 class AirlabApplication
 
 fun main(args: Array<String>) {
-	runApplication<AirlabApplication>(*args)
+	try {
+		runApplication<AirlabApplication>(*args)
+	} catch (e: Exception) {
+		e.printStackTrace()
+	}
 }
